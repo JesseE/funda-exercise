71a12093b241ff354dd4ee9c18982da4dc552e88
Jesse Eikema <jesse@voorhoede.nl>
2019-11-30 16:13:22 +0100

    render fetched data from funda to pageheader

--- components/page-header/page-header.vue
+++ components/page-header/page-header.vue
@@ -2,11 +2,28 @@
 	<header class="page-header">
 		<h1>Jesse Eikema - Frontend Developer</h1>
 		<h2>Contact - jesse.eikema1991@gmail.com</h2>
+		<div>
+			{{this.data}}
+		</div>
 	</header>
+
+
 </template>
 
 <script>
+import { getData } from '../../lib/getData'
+
 export default {
-	name: 'pageHeader'
+	name: 'pageHeader',
+	data() {
+		return {
+			data: null
+		}
+	},
+	mounted() {
+		getData().then(result => {
+			return this.data = result
+		})
+	}
 }
 </script>
