8f24c77e166792712549c7ac7435f4398e2c6cf9
Jesse Eikema <jesse@voorhoede.nl>
2019-11-30 14:01:45 +0100

    clean up nuxt scaffolding

---
+++ components/page-header/page-header.vue
@@ -0,0 +1,12 @@
+<template>
+	<header class="page-header">
+		<h1>Jesse Eikema - Frontend Developer</h1>
+		<h2>Contact - jesse.eikema1991@gmail.com</h2>
+	</header>
+</template>
+
+<script>
+export default {
+	name: 'pageHeader'
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,48 +1,13 @@
 <template>
   <main class="main">
-    <header>
-
-    </header>
-    <section>
-      <h1 class="title">Jesse Eikema <br> Frontend Developer</h1>
-      <h2 class="subtitle">contact: jesse.eikema@gmail.com</h2>
-    </section>
-    <section>
-      <article>
-        <h3>De voorhoede website</h3>
-        <img src="" alt="">
-      </article>
-      <article>
-        <h3>Hike One website</h3>
-        <img src="" alt="">
-      </article>
-      <article>
-        <h3>Say hello Mentos Machine Learing prototype</h3>
-        <img src="" alt="">
-      </article>
-      <article>
-        <h3>Bungalowspecials Mobile website</h3>
-        <img src="" alt="">
-      </article>
-      <article>
-        <h3>Hotelspecials NL release</h3>
-        <img src="" alt="">
-      </article>
-      <article>
-        <h3>NTR studioklokhuis website</h3>
-        <img src="" alt="">
-      </article>
-    </section>
-    <footer>
-
-    </footer>
+    <pageHeader />
   </main>
 </template>
 
 <script>
   export default {
       components: {
-
+        pageHeader: () => import('../components/page-header/page-header')
       },
   };
 </script>
@@ -57,27 +22,5 @@
   justify-content: center;
   align-items: center;
   text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
 }
 </style>
