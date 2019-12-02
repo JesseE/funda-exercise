c0dbbe8405bc7a1e79dc6e078ecf26eb0c2dcfb3
Jesse Eikema <jesse@voorhoede.nl>
2019-12-02 15:42:51 +0100

    done

--- src/components/text-block/text-block.vue
+++ src/components/text-block/text-block.vue
@@ -1,7 +1,11 @@
 <template>
   <article class="text-block">
     <h3 class="text-block__title">{{ this.$props.title }}</h3>
-    <p ref="textBlock" :class="this.showMore ? 'text-block__text text-block__overflow' : 'text-block__text'">{{ this.$props.summary }}</p>
+    <p
+      ref="textBlock"
+      :class="this.showMore ? 'text-block__text text-block__overflow' : 'text-block__text'">
+        {{ this.$props.summary }}
+    </p>
     <button v-if="this.showMore" class="text-block__button" @click="toggleShowMore">Lees meer</button>
     <button v-if="!this.showMore" class="text-block__button" @click="toggleShowMore">Lees minder</button>
   </article>
