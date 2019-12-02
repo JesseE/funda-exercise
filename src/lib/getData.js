27937dbba51987ac14ec56364e9b0d20de010bde
Jesse Eikema <jesse@voorhoede.nl>
2019-12-02 15:39:24 +0100

    last commit

--- src/lib/getData.js
+++ src/lib/getData.js
@@ -1,20 +1,7 @@
 import axios from 'axios';
 
 function getProjectData(projectId) {
-  const config = {
-    mode: 'no-cors',
-		headers: {
-			'Access-Control-Allow-Origin': '*',
-			Accept: 'application/json',
-			'Content-Type': 'application/json',
-		},
-		withCredentials: true,
-		credentials: 'same-origin',
-		crossdomain: true,
-  }
-
-
-	return fetch(`https://partnerapi.funda.nl/feeds/Aanbod.svc/json/detail/${process.env.TOKEN}/koop/${projectId}/`, config)
+  return axios.get(`/api/json/detail/${process.env.TOKEN}/koop/${projectId}/`)
 }
 
 export { getProjectData }
