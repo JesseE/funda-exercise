2892c307a7d145820706c3f4155056694f107b5a
Jesse Eikema <jesse@voorhoede.nl>
2019-12-02 15:18:28 +0100

    headers

--- src/lib/getData.js
+++ src/lib/getData.js
@@ -1,15 +1,14 @@
 import axios from 'axios';
 
 function getProjectData(projectId) {
-  const headerConfig = {
+  const config = {
     headers: {
-      'Access-Control-Allow-Origin':'*',
-      'host': window.location.host,
-      'credentials': false,
-      'mode': 'no-cors'
+      'Access-Control-Allow-Origin': '*'
     }
   }
-	return axios.get(`https://partnerapi.funda.nl/feeds/Aanbod.svc/json/detail/${process.env.TOKEN}/koop/${projectId}/`, headerConfig.headers)
+
+
+	return axios.get(`https://partnerapi.funda.nl/feeds/Aanbod.svc/json/detail/${process.env.TOKEN}/koop/${projectId}/`, config)
 }
 
 export { getProjectData }
